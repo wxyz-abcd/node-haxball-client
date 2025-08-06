7e13fba79d7d87d096fadf0ab6b83c90d4b3242a
jerryoldson <ivanmatus940@gmail.com>
2025-08-06 05:22:23 -0300

    see changes in live

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const API = require("node-haxball")();
-nw.Window.open('dist/index.html', {}, function(win) {
+
+const isDev = nw.App.argv[0] === "development";
+const openPath = isDev ? 'http://localhost:5173' : 'dist/index.html';
+
+nw.Window.open(openPath, {}, function(win) {
     win.window.API = API;
 });
