9efaa1e2a06bd7cd1bcc1b87532f66c1afffebe2
la cona <ivanmatus940@gmail.com>
2026-04-16 05:37:22 -0300

    use chromium built-in WebRTC instead of @koush/wrtc

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 /* eslint-disable no-undef */
-const { RTCPeerConnection, RTCIceCandidate, RTCSessionDescription } = require("@koush/wrtc");
-const path = require("path");
-
-const API = require("node-haxball")({
-      RTCPeerConnection: RTCPeerConnection, 
-      RTCIceCandidate: RTCIceCandidate, 
-      RTCSessionDescription: RTCSessionDescription, 
-});
+const WebSocket = require("ws");
 
 const isDev = nw.App.argv.includes("development");
 const openPath = isDev
   ? "http://localhost:5173"
   : "dist/index.html";
 nw.Window.open(openPath, {}, function(win) {
-    win.window.API = API;
+    const w = win.window;
+    const API = require("node-haxball")({
+        RTCPeerConnection: w.RTCPeerConnection,
+        RTCIceCandidate: w.RTCIceCandidate,
+        RTCSessionDescription: w.RTCSessionDescription,
+        fetch: w.fetch.bind(w),
+        WebSocket: WebSocket,
+        crypto: w.crypto,
+        setTimeout: w.setTimeout.bind(w),
+        clearTimeout: w.clearTimeout.bind(w),
+        setInterval: w.setInterval.bind(w),
+        clearInterval: w.clearInterval.bind(w),
+        requestAnimationFrame: w.requestAnimationFrame.bind(w),
+        cancelAnimationFrame: w.cancelAnimationFrame.bind(w),
+        performance: w.performance,
+        console: w.console,
+    });
+    w.API = API;
 });
